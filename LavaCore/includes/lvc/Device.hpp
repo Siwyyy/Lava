470f9e060740c4ec35ca6480735ebe198646e7c6
Siwy <50460157+Siwyyy@users.noreply.github.com>
2024-01-07 12:00:09 +0100

    code refactorication (again)

--- LavaCore/includes/lvc/Device.hpp
+++ LavaCore/includes/lvc/Device.hpp
@@ -1,74 +1,38 @@
 #pragma once
 
+#include "QueueFamilyIndices.hpp"
 #include "lvc/Instance.hpp"
 #include "lvc/Window.hpp"
 
-#include <optional>
 #include <string>
 #include <vector>
 #include <vulkan/vulkan.h>
 
-#include "DeviceExtensions.hpp"
-
 namespace lvc
 {
+	class DeviceExtensions;
+	class PhysicalDevice;
+
 	class Device
 	{
-	private:
-		struct QueueFamilyIndices
-		{
-			std::optional<uint32_t> graphics_family;
-			std::optional<uint32_t> present_family;
-
-			bool isComplete() const { return graphics_family.has_value() && present_family.has_value(); }
-		};
-
-		class PhysicalDevice
-		{
-		public:
-			PhysicalDevice(const VkPhysicalDevice* physical, const Window& window);
-			~PhysicalDevice();
-
-			inline const uint32_t& score() const { return m_score; }
-			inline const VkPhysicalDevice* physical() const { return m_physical; }
-			inline const DeviceExtensions* extensions() const { return m_extensions; }
-			inline const QueueFamilyIndices* indices() const { return m_indices; }
-
-			void logInfo() const;
-
-		private:
-			const VkPhysicalDevice* m_physical;
-			std::string m_name;
-			DeviceExtensions* m_extensions;
-			QueueFamilyIndices* m_indices;
-			uint32_t m_score;
-
-			void rateDeviceSuitability(const Window& window);
-			void findQueueFamilies(VkSurfaceKHR surface);
-		};
-
 	public:
-		Device(const Instance* instance, const Window* window);
+		Device(Instance* instance, Window* window);
 		~Device();
 
-		const VkPhysicalDevice* hPhysical() const { return m_physical; }
-		VkDevice hLogical() const { return m_logical; }
-		const QueueFamilyIndices* hFamilyIndices() const { return m_family_indices; }
+		inline PhysicalDevice* physicalDevice() const { return m_physical; }
+		inline VkDevice hLogical() const { return m_logical; }
 
 	private:
-		std::vector<PhysicalDevice*> m_physical_devices;
+		Instance* m_instance;
+		Window* m_window;
 
-		const VkPhysicalDevice* m_physical;
-		const DeviceExtensions* m_device_extensions;
-		const QueueFamilyIndices* m_family_indices;
+		std::vector<PhysicalDevice*> m_physical_devices;
+		PhysicalDevice* m_physical;
 
 		VkDevice m_logical;
 
 		VkQueue m_graphics_queue;
 		VkQueue m_present_queue;
-
-		const Instance* m_instance;
-		const Window* m_window;
 
 		void pickPhysicalDevice();
 	};
--- LavaCore/includes/lvc/Window.hpp
+++ LavaCore/includes/lvc/Window.hpp
@@ -1,28 +1,27 @@
 #pragma once
 
-#include "Instance.hpp"
-
 #define VK_USE_PLATFORM_WIN32_KHR
 #define GLFW_INCLUDE_VULKAN
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <glfw3.h>
-#include <glfw3native.h>
 
 namespace lvc
 {
+	class Instance;
+
 	class Window
 	{
 	public:
-		Window(const int& width, const int& height, const char* title, const Instance* instance);
+		Window(const int& width, const int& height, const char* title, Instance* instance);
 		Window() = delete;
 		~Window();
 
 		inline GLFWwindow* handle() const { return m_window; }
-		inline VkSurfaceKHR hSurface() const { return m_surface; }
+		inline VkSurfaceKHR surfaceHandle() const { return m_surface; }
 
 	private:
 		GLFWwindow* m_window;
 		VkSurfaceKHR m_surface;
-		const Instance* m_instance;
+		Instance* m_instance;
 	};
 }
