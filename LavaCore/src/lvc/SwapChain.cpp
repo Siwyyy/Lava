3f104f067afca7ef8aa1f76746967ebed7437489
Siwy <50460157+Siwyyy@users.noreply.github.com>
2024-01-09 19:21:44 +0100

    added image views creation

--- LavaCore/src/lvc/SwapChain.cpp
+++ LavaCore/src/lvc/SwapChain.cpp
@@ -27,8 +27,71 @@ SwapChain::SwapChain(Device* device, Window* window)
 
 SwapChain::~SwapChain()
 {
+	for (const VkImageView& image_view : m_image_views)
+		vkDestroyImageView(m_device, image_view, nullptr);
 	vkDestroySwapchainKHR(m_device, m_swapchain, nullptr);
 	std::clog << "Successfully destroyed swapchain\n";
+}
+
+void SwapChain::querySwapchainSupport()
+{
+	// Extent
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(m_physical_device, m_surface, &m_surface_capabilities);
+
+	// Surface format
+	uint32_t format_count;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(m_physical_device, m_surface, &format_count, nullptr);
+	m_surface_formats.resize(format_count);
+	vkGetPhysicalDeviceSurfaceFormatsKHR(m_physical_device, m_surface, &format_count, m_surface_formats.data());
+
+	// Surface present mode
+	uint32_t mode_count;
+	vkGetPhysicalDeviceSurfacePresentModesKHR(m_physical_device, m_surface, &mode_count, nullptr);
+	m_present_modes.resize(mode_count);
+	vkGetPhysicalDeviceSurfacePresentModesKHR(m_physical_device, m_surface, &mode_count, m_present_modes.data());
+
+	if (m_surface_formats.empty() || m_present_modes.empty())
+		throw::std::runtime_error("err: Device details not supported! SWAPCHAIN cannot be created!");
+}
+
+void SwapChain::setExtent2D()
+{
+	if (m_surface_capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+	{
+		m_extent_2d = m_surface_capabilities.currentExtent;
+		return;
+	}
+
+	int width_int, height_int;
+	glfwGetFramebufferSize(m_window, &width_int, &height_int);
+
+	const uint32_t width  = std::clamp(static_cast<uint32_t>(width_int), m_surface_capabilities.minImageExtent.width, m_surface_capabilities.maxImageExtent.width);
+	const uint32_t height = std::clamp(static_cast<uint32_t>(height_int), m_surface_capabilities.minImageExtent.height, m_surface_capabilities.maxImageExtent.height);
+
+	m_extent_2d = {width,height};
+}
+
+void SwapChain::setSurfaceFormat()
+{
+	for (const auto& format : m_surface_formats)
+		if (format.format == VK_FORMAT_B8G8R8A8_SRGB && format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+		{
+			m_surface_format = format;
+			return;
+		}
+
+	m_surface_format = m_surface_formats[0];
+}
+
+void SwapChain::setSurfacePresentMode()
+{
+	for (const auto& mode : m_present_modes)
+		if (mode == VK_PRESENT_MODE_MAILBOX_KHR)
+		{
+			m_present_mode = mode;
+			return;
+		}
+	m_present_mode = VK_PRESENT_MODE_FIFO_KHR;
 }
 
 void SwapChain::createSwapchain()
@@ -71,67 +134,33 @@ void SwapChain::createSwapchain()
 		throw std::runtime_error("err: Failed to create swapchain!\n");
 
 	vkGetSwapchainImagesKHR(m_device, m_swapchain, &image_count, nullptr);
-	m_image_vec.resize(image_count);
-	vkGetSwapchainImagesKHR(m_device, m_swapchain, &image_count, m_image_vec.data());
+	m_images.resize(image_count);
+	vkGetSwapchainImagesKHR(m_device, m_swapchain, &image_count, m_images.data());
 }
 
-void SwapChain::querySwapchainSupport()
+void SwapChain::createImageViews()
 {
-	// Extent
-	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(m_physical_device, m_surface, &m_surface_capabilities);
-
-	// Surface format
-	uint32_t format_count;
-	vkGetPhysicalDeviceSurfaceFormatsKHR(m_physical_device, m_surface, &format_count, nullptr);
-	m_surface_format_vec.resize(format_count);
-	vkGetPhysicalDeviceSurfaceFormatsKHR(m_physical_device, m_surface, &format_count, m_surface_format_vec.data());
-
-	// Surface present mode
-	uint32_t mode_count;
-	vkGetPhysicalDeviceSurfacePresentModesKHR(m_physical_device, m_surface, &mode_count, nullptr);
-	m_present_mode_vec.resize(mode_count);
-	vkGetPhysicalDeviceSurfacePresentModesKHR(m_physical_device, m_surface, &mode_count, m_present_mode_vec.data());
-
-	if (m_surface_format_vec.empty() || m_present_mode_vec.empty())
-		throw::std::runtime_error("err: Device details not supported! SWAPCHAIN cannot be created!");
-}
-
-void SwapChain::setExtent2D()
-{
-	if (m_surface_capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+	m_image_views.resize(m_images.size());
+	for (size_t i = 0; i < m_image_views.size(); i++)
 	{
-		m_extent_2d = m_surface_capabilities.currentExtent;
-		return;
+		VkImageViewCreateInfo create_info{};
+		create_info.sType                           = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		create_info.image                           = m_images[i];
+		create_info.viewType                        = VK_IMAGE_VIEW_TYPE_2D;
+		create_info.format                          = m_surface_format.format;
+		create_info.components.r                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.components.g                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.components.b                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.components.a                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.subresourceRange.aspectMask     = VK_IMAGE_ASPECT_COLOR_BIT;
+		create_info.subresourceRange.baseMipLevel   = 0;
+		create_info.subresourceRange.levelCount     = 1;
+		create_info.subresourceRange.baseArrayLayer = 0;
+		create_info.subresourceRange.layerCount     = 1;
+
+		if (vkCreateImageView(m_device, &create_info, nullptr, &m_image_views[i]) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create image views!");
+		}
 	}
-
-	int width_int, height_int;
-	glfwGetFramebufferSize(m_window, &width_int, &height_int);
-
-	const uint32_t width  = std::clamp(static_cast<uint32_t>(width_int), m_surface_capabilities.minImageExtent.width, m_surface_capabilities.maxImageExtent.width);
-	const uint32_t height = std::clamp(static_cast<uint32_t>(height_int), m_surface_capabilities.minImageExtent.height, m_surface_capabilities.maxImageExtent.height);
-
-	m_extent_2d = {width,height};
-}
-
-void SwapChain::setSurfaceFormat()
-{
-	for (const auto& format : m_surface_format_vec)
-		if (format.format == VK_FORMAT_B8G8R8A8_SRGB && format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
-		{
-			m_surface_format = format;
-			return;
-		}
-
-	m_surface_format = m_surface_format_vec[0];
-}
-
-void SwapChain::setSurfacePresentMode()
-{
-	for (const auto& mode : m_present_mode_vec)
-		if (mode == VK_PRESENT_MODE_MAILBOX_KHR)
-		{
-			m_present_mode = mode;
-			return;
-		}
-	m_present_mode = VK_PRESENT_MODE_FIFO_KHR;
 }
